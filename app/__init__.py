83a636cdecfdbc6515cc2977a110342b2075bb16
Noel Niles <noelniles@gmail.com>
2017-12-26 08:25:47 +0000

    add sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 from app import config
 
 app = Flask(__name__)
 app.config.from_object(config.Config)
+
+db = SQLAlchemy(app)
 
 from app import routes
--- app/config.py
+++ app/config.py
@@ -2,4 +2,7 @@ import os
 
 
 class Config:
+    basedir = os.path.abspath(os.path.dirname(__file__))
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.path.join('sqlite:///', basedir, 'app.db')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'dbrepo')
