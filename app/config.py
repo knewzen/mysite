ecce921b0a440a1019a031033117a5a53d7f1b30
Noel Niles <noelniles@gmail.com>
2017-12-26 09:03:30 -1000

    Add posts table

--- app/config.py
+++ app/config.py
@@ -4,5 +4,6 @@ import os
 class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.path.join('sqlite:///', basedir, 'app.db')
-    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'dbrepo')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
+        or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
